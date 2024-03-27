48db1168e043cf38657f88acb16281f814ecbaf1
Mathavan208 <210701154@rajalakshmi.edu.in>
2024-03-27 18:07:41 +0530

    counter-react

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,28 +1,64 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-class Counter extends Component{
-	state={
-		count:0,
-		formatcount(){
-			return this.count===0?<h1>Zero</h1>:this.count;
-		},
-		
-	};
-	styles={
-		fontWeight:"bold"
-	}
-	getbadges(){
-		let classes="btn m-2 btn-"
-		 classes+=(this.count===0)?"warning":"primary";
-		 return classes;
-	}
-	render(){
-		// let classes="btn m-2 btn-"
-		//  classes+=(this.count===0)?"warning":"primary";
-		// console.log(classes);
-		return (<React.Fragment><span style={this.styles} className={this.getbadges()}>{this.state.formatcount()}</span>
-		<button className="btn btn-secondary btn-sm">Increement</button>
-</React.Fragment>);//we cannot add the another element in this since jsx cannot compiled
-	};
-};
+class Counter extends Component {
+    constructor(){
+        super();
+        this.handleEvent=this.handleEvent.bind(this);
+    }
+    state = {
+        count: 0,
+        image: './public/tree.jpg',
+        tags:['red','green','blue']
+    };
+
+    formatcount() {
+        return this.state.count === 0 ? <h1>Zero</h1> : this.state.count;
+    }
+
+    styles = {
+        fontWeight: "bold"
+    };
+
+
+handleEvent(product)
+{
+    console.log(product);
+    console.log("increemented",this.state.count++);
+    this.setState({count:this.state.count++});
+}
+
+
+
+
+
+    getbadges() {
+        let classes = "btn m-2 btn-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+renderTags(){
+    if(this.state.tags.length===0) return <p>No tags are present</p>
+
+return (<ul>
+                       {this.state.tags.map(tags=> <li key={tags}>{tags}</li>  )}
+                </ul>);
+}
+
+
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <span style={this.styles} className={this.getbadges()}>
+                    {this.formatcount()}
+                </span>
+                <button className="btn btn-secondary btn-sm" onClick={()=> this.handleEvent({id:1})}>Increment</button>
+                {this.state.tags.length===0&&"no tags are present"}
+                {this.renderTags()}
+            </React.Fragment>
+        );
+    }
+}
+
 export default Counter;
